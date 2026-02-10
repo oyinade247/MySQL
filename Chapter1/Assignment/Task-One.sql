e85e1edafb05172c8247fec8dc1081b6f403a0f5
Your Name <you@example.com>
2026-02-10 07:26:06 +0100

    Sql Assignment

--- Chapter1/Assignment/Task-One.sql
+++ Chapter1/Assignment/Task-One.sql
@@ -1,9 +1,29 @@
-CREATE DATABASE user_db;
-USE user_db;
+CREATE DATABASE ecommerce;
+USE ecommerce;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE
 );
-SHOW TABLES;
+
+CREATE TABLE products(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    price DECIMAL(10,2),
+    stock INT
+);
+
+CREATE TABLE orders(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    product_id INT,
+    quantity INT,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+)
+
+SHOW DATABASES;
+
+
